25b96efa326f16a38be93007cdcae18e12c0153b
MacOs <macos@192.168.1.5>
2025-01-28 13:23:29 -0300

    Feat: add menseger of confirmed password

--- FrontEnd/PetShop/src/pages/cadastroClient.jsx
+++ FrontEnd/PetShop/src/pages/cadastroClient.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function cadastroClient() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    alert("Enviou o formulario.");
+    if (
+      password != "" &&
+      confirmPassword != "" &&
+      password === confirmPassword
+    ) {
+      setError(""); // Limpa erros anteriores
+      alert("Senha confirmada com sucesso!"); // Exibe uma mensagem de sucesso
+    } else {
+      setError("As senhas não coincidem. Por favor, tente novamente.");
+    }
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base/7 font-semibold text-gray-900">
             Personal Information
@@ -25,7 +45,6 @@ function cadastroClient() {
                   type="text"
                   name="first-name"
                   id="first-name"
-                  autocomplete="given-name"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -43,7 +62,6 @@ function cadastroClient() {
                   type="text"
                   name="last-name"
                   id="last-name"
-                  autocomplete="family-name"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -60,8 +78,7 @@ function cadastroClient() {
                 <input
                   id="email"
                   name="email"
-                  type="email"
-                  autocomplete="email"
+                  type="text"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -80,8 +97,7 @@ function cadastroClient() {
                 <input
                   id="email"
                   name="email"
-                  type="email"
-                  autocomplete="email"
+                  type="text"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -98,7 +114,6 @@ function cadastroClient() {
                   id="email"
                   name="email"
                   type="text"
-                  autocomplete="email"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -117,7 +132,6 @@ function cadastroClient() {
                   id="email"
                   name="email"
                   type="email"
-                  autocomplete="email"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -134,7 +148,6 @@ function cadastroClient() {
                   id="email"
                   name="email"
                   type="text"
-                  autocomplete="email"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -152,7 +165,6 @@ function cadastroClient() {
                   id="email"
                   name="email"
                   type="email"
-                  autocomplete="email"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -169,7 +181,6 @@ function cadastroClient() {
                   id="email"
                   name="email"
                   type="text"
-                  autocomplete="email"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -187,7 +198,6 @@ function cadastroClient() {
                   type="text"
                   name="first-name"
                   id="first-name"
-                  autocomplete="given-name"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -205,7 +215,6 @@ function cadastroClient() {
                   type="text"
                   name="last-name"
                   id="last-name"
-                  autocomplete="family-name"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -223,7 +232,9 @@ function cadastroClient() {
                   type="text"
                   name="street-address"
                   id="street-address"
-                  autocomplete="street-address"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -236,16 +247,24 @@ function cadastroClient() {
               >
                 <span>confirmação</span> da senha
               </label>
-              <div className="mt-2">
+              <div id="confirPassword" className="mt-2">
                 <input
                   type="text"
                   name="city"
                   id="city"
-                  autocomplete="address-level2"
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                  }}
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
             </div>
+            {error && <div className='sm:col-span-full'>
+               <span
+               for="city"
+               className="block text-sm/6 font-medium text-red-900"
+               >{error}</span>
+           </div>}
           </div>
         </div>
 

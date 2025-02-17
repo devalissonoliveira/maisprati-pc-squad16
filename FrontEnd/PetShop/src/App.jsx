9ec5c2b4ba3a8d9eedaebdd4e29911f1ed936a2f
MacOs <macos@192.168.1.6>
2025-02-17 09:50:50 -0300

    Feat:added responsiveness to login and hamburger menu

--- FrontEnd/PetShop/src/App.jsx
+++ FrontEnd/PetShop/src/App.jsx
@@ -1,16 +1,134 @@
 import { useState } from "react";
 import "./App.css";
 import Login from "./pages/login";
-import Register  from "./pages/petRegistration";
-import CadastroClient  from "./pages/cadastroClient";
+import Register from "./pages/petRegistration";
+import CadastroClient from "./pages/cadastroClient";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <>
-      {/* <Login /> */}
-      <Register/>
+      <section>
+        <div className="flex items-center justify-between">
+          <div className="text-lg font-bold">
+            <a href="#logo" className="hover:text-blue-700">
+              MinhaLogo
+            </a>
+          </div>
+          <nav className="flex items-center pl-10 justify-between text-blue-950 max-sm:hidden">
+            {/* Links do lado esquerdo */}
+            <ul className="flex text-end space-x-6">
+              <li>
+                <a href="#home" className="hover:text-blue-700">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#como-funciona" className="hover:text-blue-700">
+                  Como funciona
+                </a>
+              </li>
+              <li>
+                <a href="#plano" className="hover:text-blue-700">
+                  Plano
+                </a>
+              </li>
+              <li>
+                <a href="#contato" className="hover:text-blue-700">
+                  Contato
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="md:hidden">
+            <button onClick={toggleMenu}>
+              {/* Exibe o ícone de hambúrguer ou "X" dependendo do estado */}
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          {/* section menu escondido */}
+          {isMenuOpen && (
+            <aside
+              className={`fixed inset-0 z-50 transform transition-transform duration-300 ease-in-out ${
+                isMenuOpen ? "translate-x-0" : "-translate-x-full"
+              } md:hidden`}
+            >
+              <div className="absolute top-5 left-5">
+                <button
+                  onClick={toggleMenu}
+                  id="closeHamburgue"
+                  className="text-black"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+              <ul className="flex flex-col space-x-10 space-y-5 pt-6 pb-20 px-6 bg-slate-400">
+                <li className="m-0">
+                  <a href="#home" className="hover:text-blue-700 text-center">
+                    Home
+                  </a>
+                </li>
+                <li className="m-0">
+                  <a
+                    href="#como-funciona"
+                    className="hover:text-blue-700 text-center"
+                  >
+                    Como funciona
+                  </a>
+                </li>
+                <li className="m-0 bg-red-500">
+                  <a href="#plano" className="hover:text-blue-700 text-center">
+                    Plano
+                  </a>
+                </li>
+                <li className="m-0">
+                  <a
+                    href="#contato"
+                    className="hover:text-blue-700 text-center"
+                  >
+                    Contato
+                  </a>
+                </li>
+              </ul>
+            </aside>
+          )}
+          {/* section menu escondido */}
+        </div>
+      </section>
+      <Login />
+      {/* <Register/> */}
       {/* <CadastroClient/> */}
     </>
   );
--- FrontEnd/PetShop/src/pages/login.jsx
+++ FrontEnd/PetShop/src/pages/login.jsx
@@ -1,123 +1,72 @@
 // import { Container } from './styles';
 
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
 function Login() {
   return (
     <>
-      {/* Logo do lado direito */}
-      <section className="flex flex-col">
-        <div className="flex items-center">
-          <div className="text-lg font-bold">
-            <a href="#logo" className="hover:text-blue-700">
-              MinhaLogo
-            </a>
-          </div>
-          <nav className="flex items-center justify-between px-6 py-4 text-blue-950">
-            {/* Links do lado esquerdo */}
-            <ul className="flex space-x-6">
-              <li>
-                <a href="#home" className="hover:text-blue-700">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#como-funciona" className="hover:text-blue-700">
-                  Como funciona
-                </a>
-              </li>
-              <li>
-                <a href="#plano" className="hover:text-blue-700">
-                  Plano
-                </a>
-              </li>
-              <li>
-                <a href="#contato" className="hover:text-blue-700">
-                  Contato
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      {/* Section de login */}
+      <form method="post" onSubmit={handleSubmit}>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <h2 className="capitalize text-2xl font-semibold text-blue-700">
+            Login
+          </h2>
 
-        {/* Section de login */}
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-          <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-
-          <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
-              <div>
-                <label
-                  for="email"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
-                  User address
-                </label>
-                <div class="mt-2">
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    autocomplete="email"
-                    required
-                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div class="flex items-center justify-between">
-                  <label
-                    for="password"
-                    class="block text-sm/6 font-medium text-gray-900"
-                  >
-                    Password
-                  </label>
-                  <div class="text-sm">
-                    <a
-                      href="#"
-                      class="font-semibold text-indigo-600 hover:text-blue-700"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div class="mt-2">
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    autocomplete="current-password"
-                    required
-                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-
-            <p class="mt-10 text-center text-sm/6 text-gray-500">
-              Not a member?
-              <a
-                href="#"
-                class="font-semibold text-indigo-600 hover:text-indigo-500"
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-5 sm:grid-cols-6">
+            <div className="sm:col-span-full">
+              <label
+                for="user"
+                className="text-start block text-sm/6 font-medium text-gray-900"
               >
-                Register
-              </a>
-            </p>
+                User address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="user"
+                  name="user"
+                  type="text"
+                  required
+                  placeholder="Your name user is here"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-full">
+              <label
+                for="password"
+                className="text-start block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  equired
+                  placeholder="Your password is here"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <article className="sm:col-span-full mt-0 text-blue-400 font-normal text-end">
+              <a href="#">esqueceu sua senha?</a>
+            </article>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6 sm:col-span-full">
+              <button
+                type="submit"
+                className="rounded-md bg-blue-600 px-3 py-2 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 capitalize w-full text-lg"
+              >
+                entrar
+              </button>
+            </div>
           </div>
         </div>
-      </section>
+      </form>
     </>
   );
 }
